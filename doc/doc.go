3e86eaad50a503d4d997927337de0823bd37bc4d
Will Alexander <wwalexander@users.noreply.github.com>
2021-07-06 16:23:11 -0400

    Add CircleCI badge to README

--- doc/doc.go
+++ doc/doc.go
@@ -9,11 +9,11 @@ import (
 	"github.com/spf13/cobra/doc"
 )
 
-// TODO: Add CircleCI badge after first public release
 const fm = `Thermite
 ========
 
 [![Go Reference](https://pkg.go.dev/badge/github.com/dollarshaveclub/thermite.svg)](https://pkg.go.dev/github.com/dollarshaveclub/thermite)
+[![CircleCI](https://circleci.com/gh/dollarshaveclub/thermite/tree/master.svg?style=svg)](https://circleci.com/gh/dollarshaveclub/thermite/tree/master)
 
 	go get github.com/dollarshaveclub/thermite
 
