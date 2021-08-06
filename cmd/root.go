2d618a541b51811d76b2340bab930425e829a00d
Will Alexander <wwalexander@users.noreply.github.com>
2021-08-05 22:12:43 -0400

    Update Helm chart version

--- cmd/root.go
+++ cmd/root.go
@@ -130,7 +130,7 @@ func run(logger *log.Logger) (pruned []string, err error) {
 }
 
 var RootCmd = &cobra.Command{
-	Version: "0.0.22",
+	Version: "0.0.23",
 	Use:     "thermite",
 	Short:   "Remove old and undeployed Amazon Elastic Container Registry images",
 	Long: `Thermite removes old Amazon Elastic Container Registry images that are not
