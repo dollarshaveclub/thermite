98b0b3ce8f26a7f91ff51080f71dc846d51da83d
Will Alexander <wwalexander@users.noreply.github.com>
2021-08-10 21:29:38 -0400

    More version juggling

--- cmd/root.go
+++ cmd/root.go
@@ -130,7 +130,7 @@ func run(logger *log.Logger) (pruned []string, err error) {
 }
 
 var RootCmd = &cobra.Command{
-	Version: "0.0.23",
+	Version: "0.0.27",
 	Use:     "thermite",
 	Short:   "Remove old and undeployed Amazon Elastic Container Registry images",
 	Long: `Thermite removes old Amazon Elastic Container Registry images that are not
